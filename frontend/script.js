2843233fe04dcf34675b0496555da21b2abb1ba4
kasimxo <kasimxo@gmail.com>
2025-10-25 19:39:46 +0200

    small improvement

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
 const CANVAS_WIDTH = 400
 const CANVAS_HEIGHT = 400
 const GRID_LINES_SPACE = 25
+const GRID_MARGINS = 25
 
 let ctx;
 let dataPoints = []
@@ -17,14 +18,23 @@ function InitCanvas() {
 
 function DrawGridLines(){
     ctx.beginPath();
-    for(let y = 0; y<CANVAS_HEIGHT; y+=GRID_LINES_SPACE){
-        ctx.moveTo(0, y)
+    ctx.strokeStyle = "grey";
+    ctx.font = "15px serif";
+    
+    ctx.textAlign = 'right'
+    ctx.textBaseline = 'middle'
+    for(let y = GRID_MARGINS; y<CANVAS_HEIGHT; y+=GRID_LINES_SPACE){
+        ctx.moveTo(GRID_MARGINS, y)
         ctx.lineTo(CANVAS_WIDTH, y);
+        if(y%(GRID_MARGINS*2)) ctx.fillText(`${y-GRID_MARGINS}`, GRID_MARGINS, CANVAS_HEIGHT-y);
     }
     
-    for(let x = 0; x<CANVAS_WIDTH; x+= GRID_LINES_SPACE){
+    ctx.textAlign = 'center'    
+    ctx.textBaseline = 'top'
+    for(let x = GRID_MARGINS; x<CANVAS_WIDTH; x+= GRID_LINES_SPACE){
         ctx.moveTo(x, 0)
-        ctx.lineTo(x, CANVAS_HEIGHT);
+        ctx.lineTo(x, CANVAS_HEIGHT-GRID_MARGINS);
+        if(x%(GRID_MARGINS*2)!==0) ctx.fillText(`${x-GRID_MARGINS}`, x, CANVAS_HEIGHT-GRID_MARGINS);
     }
     ctx.stroke();
     ctx.closePath();
